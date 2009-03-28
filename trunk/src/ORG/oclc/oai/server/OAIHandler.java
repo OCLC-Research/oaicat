6a6ff7fc1d5f9facbcc29e11fe7926cab138d0ea
jyoung.oclc <jyoung.oclc@5ee376a2-a430-11dd-9a14-c1645b63b0c6>
2009-03-28 01:53:59 +0000

    Fix persistentConnection.

--- trunk/src/ORG/oclc/oai/server/OAIHandler.java
+++ trunk/src/ORG/oclc/oai/server/OAIHandler.java
@@ -58,7 +58,7 @@ public class OAIHandler extends HttpServlet {
     
     public static final String PROPERTIES_SERVLET_CONTEXT_ATTRIBUTE = OAIHandler.class.getName() + ".properties";
     
-    private static final String VERSION = "1.5.56";
+    private static final String VERSION = "1.5.57";
     private static boolean debug = false;
 
 //    private Transformer transformer = null;
